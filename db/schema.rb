b6a90a536f5ddba73a5a75fbd9a424f973d5c0f7
Marwan Belhora <marwan.belhora@audencia.com>
2022-08-17 11:15:06 +0200

    en cours

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2022_08_16_111151) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "detail"
+    t.string "details"
     t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
